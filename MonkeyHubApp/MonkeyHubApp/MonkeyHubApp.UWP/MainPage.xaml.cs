10f2998456fe202c1d12688f2771f9c543f0bb8b
Alexandre Zollinger Chohfi <azchohfi@hotmail.com>
2017-03-28 10:43:47 -0300

    Commit inicial de código.

---
+++ MonkeyHubApp/MonkeyHubApp/MonkeyHubApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MonkeyHubApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new MonkeyHubApp.App());
+        }
+    }
+}
---
+++ MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MonkeyHubApp
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
